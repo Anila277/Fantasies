ef18a139037871d6fc03aba6daf06516466abc1f
Anila Nisar <raniyasr@gmail.com>
2022-09-25 21:35:25 -0400

    wip: adds star ratings

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,53 @@
+import { GiFlexibleStar } from 'react-icons/gi';
+import { useState } from 'react';
+
+const colors = {
+    grey: "#556483",
+    green: "#63d670"
+
+}
+
 function About(props) {
 
+    const stars = Array(5).fill(0);
+
+    const [currentValue, setCurrentValue] = useState(0);
+
+    const [hoverValue, setHoverValue] = useState(null);
+
+    const handleClick = value => {
+        setCurrentValue(value)
+    };
+
+    const handleMouseOver = value => {
+        setHoverValue(value)
+    }
+
+    const handleMouseLeave = () => {
+        setHoverValue(null)
+    }
+
     return (
-        <div className="about">
-            <h1>About Our App</h1>
-            <h2>A Poetry app...</h2>
+        <div >
+            <h1>About Fantasies</h1>
+            <h3> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim distinctio ea
+                <br /> nesciunt, sapiente quibusdam fugiat, alias sunt pariatur, optio totam odit?
+                <br /> Aperiam ex dicta aspernatur suscipit tempora recusandae consequuntur officiis.</h3>
+            <div >
+                {stars.map((_, index) => {
+                    return (
+                        <GiFlexibleStar
+                            key={index}
+                            color={(hoverValue || currentValue) > index ? colors.green : colors.grey}
+                            onClick={() => handleClick(index + 1)}
+                            onMouseOver={() => handleMouseOver(index + 1)}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                    )
+                })}
+
+            </div>
+
         </div>
     )
 }
