20869f466d013df70a1c51c7cdd4166376b545bb
Anila Nisar <raniyasr@gmail.com>
2022-09-25 21:38:27 -0400

    wip: adds name of the creators

--- src/pages/About.js
+++ src/pages/About.js
@@ -51,6 +51,14 @@ function About(props) {
                 placeHolder='Leave a Review'
             />
             <button>Submit</button>
+            <div className="profile">
+                <h1>About Our Team</h1>
+                <h3>Ben Allen</h3>
+                <h3>Dudley Gamino</h3>
+                <h3>Samuel Johnson</h3>
+                <h3>Anila Nisar</h3>
+
+            </div>
         </div>
     )
 }
