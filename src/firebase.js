114ba6bb026854a918350c6a52a619ed7ccea0be
Anila Nisar <raniyasr@gmail.com>
2022-09-26 19:17:27 -0400

    wip: imports signOut, getAuth, signInWithPopup, GoogleAuthProvider

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+    signOut,
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +21,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export { auth, login, logout };
