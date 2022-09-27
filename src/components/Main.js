68160c83d432ce309030acbe643a7b0b5bbd137b
Anila Nisar <raniyasr@gmail.com>
2022-09-26 22:51:09 -0400

    wip: adds token and sends json web token

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,8 +16,18 @@ function Main({ user }) {
     const API_URL = 'http://localhost:4000/api/poems'
 
     const getData = async () => {
+
+        if (!user) return; // do not run this function unless a user is logged in
+
         try {
-            const response = await fetch(API_URL);
+
+            const token = await user.getIdToken();
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            });
             const data = await response.json();
             setPoems(data);
         } catch (error) {
