1ec854c7d3941ad5758402fb3f4ffac7f1e3c993
Anila Nisar <raniyasr@gmail.com>
2022-09-29 11:42:57 -0400

    wip: removes the like/dislike function and imports LikeDis component

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
+import LikeDis from '../components/LikesDis';
+
 
 
 import { useParams, useNavigate } from 'react-router-dom';
@@ -7,32 +9,6 @@ function Show({ poems, deletePoems, updatePoems, user }) {
     const { id } = useParams();
     const poem = poems ? poems.find(p => p._id === id) : null;
     const navigate = useNavigate();
-
-
-    const [likes, setLikes] = useState(99);
-    const [isClicked, setIsClicked] = useState(null);
-
-    const likeButton = () => {
-        if (isClicked) {
-            setLikes(likes - 1);
-        } else {
-            setLikes(likes + 1);
-        }
-        setIsClicked(!isClicked)
-    }
-
-    const [dislikes, setDislikes] = useState(0);
-    const [isDisliked, setIsDisliked] = useState(null);
-
-    const dislikeButton = () => {
-        if (isDisliked) {
-            setDislikes(dislikes - 1);
-        } else {
-            setDislikes(dislikes + 1);
-        }
-        setIsDisliked(!isDisliked)
-    }
-
 
     const [editForm, setEditForm] = useState({
         name: '',
@@ -121,11 +97,7 @@ function Show({ poems, deletePoems, updatePoems, user }) {
 
                 }
                 <br />
-                <span className="likes-counter">{`${likes}`}</span>
-                <FaThumbsUp className={`like-button ${isClicked && 'liked'}`} onClick={likeButton} />
-
-                <span className="dislikes-counter">{`${dislikes}`}</span>
-                <FaThumbsDown className={`dislike-button ${isClicked && 'disliked'}`} onClick={dislikeButton} />
+                <div><LikeDis /></div>
 
                 <br />
                 <form onSubmit={handleComment}>
